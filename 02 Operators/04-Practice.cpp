f619cc38f056286c3df89ffcdd5e7f1c6f46f377
JunaidHassan19 <thejunni01@gmail.com>
2026-02-08 21:57:31 +0530

    Practice Qs

---
+++ 02 Operators/04-Practice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  /*
+Question: Pridic output of cout << (bool)3 + 2;
+*/
+  cout << (bool)3 + 2 << endl;
+
+  /*
+Question: Pridict output of cout << (23.5 + 2 + 'A);
+*/
+  cout << (23.5 + 2 + 'A') << endl;
+
+  return 0;
+}
