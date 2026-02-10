86721dd81bad44eb296317aed878c538206dbb74
JunaidHassan19 <thejunni01@gmail.com>
2026-02-10 21:15:40 +0530

    Practice Qs

---
+++ 03 Conditional Statements/02-PracticeQs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Print the largest of 2 numbers.
+  int num1, num2;
+  cout << "Enter first number: ";
+  cin >> num1;
+  cout << "Enter Second number: ";
+  cin >> num2;
+
+  if (num1 > num2)
+  {
+    cout << "The largest number is: " << num1;
+  }
+  else
+  {
+    cout << num2 << " Is the largest number";
+  }
+
+  // Print if a Number is Odd or Even.
+  int num;
+  cout << "Enter a Number:";
+  cin >> num;
+
+  if (num % 2 == 0)
+  {
+    cout << num << " Is Even";
+  }
+  else
+  {
+    cout << num << " Is Odd";
+  }
+
+  return 0;
+}
