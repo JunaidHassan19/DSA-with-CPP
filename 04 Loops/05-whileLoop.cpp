3a46cbca768391cb45389bbfcbdd9c486fa4fadf
JunaidHassan19 <thejunni01@gmail.com>
2026-02-12 21:54:22 +0530

    while Loop

---
+++ 04 Loops/05-whileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 1;     // Initialize counter variable to 1
+  while (i <= 5) // Loop while i is less than or equal to 5
+  {
+    cout << i << " "; // Print current value of i
+    i++;              // Increment i by 1
+  }
+  return 0; // Exit program
+}
