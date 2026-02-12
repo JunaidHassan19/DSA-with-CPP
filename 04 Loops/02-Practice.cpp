8908c857b6c3d5936d850180ee7497af19e42bf6
JunaidHassan19 <thejunni01@gmail.com>
2026-02-12 21:23:17 +0530

    Print "DSA With C++" 5 times

---
+++ 04 Loops/02-Practice.cpp
@@ -0,0 +1,22 @@
+/*
+Q : Print "DSA With C++" 5 times
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  for (int i = 1; i <= 5; i++)
+  {
+    cout << "DSA With C++" << endl;
+  }
+  return 0;
+}
+
+/*
+Output: DSA With C++
+        DSA With C++
+        DSA With C++
+        DSA With C++
+        DSA With C++
+*/
