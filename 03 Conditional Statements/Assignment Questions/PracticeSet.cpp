6ca7e14c10af165ff47e80610f7196508ae69a79
JunaidHassan19 <thejunni01@gmail.com>
2026-02-12 20:08:33 +0530

    More Practice

--- 03 Conditional Statements/Assignment Questions/PracticeSet.cpp
+++ 03 Conditional Statements/Assignment Questions/PracticeSet.cpp
@@ -31,31 +31,56 @@ int main()
   // }
   // return 0;
 
+  // int n;
+  // cout << "Enter a 3 digit number: ";
+  // cin >> n;
+
+  // int num = n;
+
+  // int dig1 = num % 10;
+  // num /= 10;
+
+  // int dig2 = num % 10;
+  // num /= 10;
+
+  // int dig3 = num;
+
+  // int cubeSum = dig1 * dig1 * dig1 +
+  //               dig2 * dig2 * dig2 +
+  //               dig3 * dig3 * dig3;
+
+  // if (cubeSum == n)
+  // {
+  //   cout << num << " Is Armstrong" << endl;
+  // }
+  // else
+  // {
+  //   cout << num << " Is NOT Armstrong" << endl;
+  // }
+
   int n;
-  cout << "Enter a 3 digit number: ";
+  cout << "Enter 3 digit number: ";
   cin >> n;
 
   int num = n;
 
-  int dig1 = num % 10;
+  int digit1 = num % 10;
   num /= 10;
-
-  int dig2 = num % 10;
+  int digit2 = num % 10;
   num /= 10;
+  int digit3 = num;
 
-  int dig3 = num;
-
-  int cubeSum = dig1 * dig1 * dig1 +
-                dig2 * dig2 * dig2 +
-                dig3 * dig3 * dig3;
+  int cubeSum = digit1 * digit1 * digit1 +
+                digit2 * digit2 * digit2 +
+                digit3 * digit3 * digit3;
 
   if (cubeSum == n)
   {
-    cout << num << " Is Armstrong" << endl;
+    cout << n << " Is" << endl;
   }
   else
   {
-    cout << num << " Is NOT Armstrong" << endl;
+    cout << n << " Is NOT";
   }
 
   return 0;
