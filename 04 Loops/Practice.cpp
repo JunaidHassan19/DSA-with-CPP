95316da72a506951f2b80acb66a67450f3bd30cf
JunaidHassan19 <thejunni01@gmail.com>
2026-02-13 12:15:50 +0530

    More Self Practice

---
+++ 04 Loops/Practice.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+// Q. Print the sum of digits of a number using while loop
+// n = 10829
+
+// int main()
+// {
+//   int n = 12345;
+//   int sum = 0;
+
+//   while (n > 0)
+//   {
+//     int lastDig = n % 10;
+//     sum = sum + lastDig;
+//     n /= 10;
+//   }
+//   cout << "sum is: " << sum;
+//   return 0;
+// }
+
+// Q. Print the Sum of odd Digits of a number using while Loop.
+// int main()
+// {
+//   int n = 123456;
+//   int sum = 0;
+
+//   while (n > 0)
+//   {
+//     int lastDig = n % 10;
+//     if (lastDig % 2 != 0)
+//     {
+//       sum += lastDig;
+//     }
+//     n /= 10;
+//   }
+//   cout << "sum of add digits: " << sum;
+// }
+
+/*
+    Qs: Print the digits of a given number in reverse using while loop
+       n = 10829
+ */
+
+// int main()
+// {
+//   int n = 987654321;
+
+//   while (n > 0)
+//   {
+//     int lastDig = n % 10;
+//     cout << lastDig;
+//     n /= 10;
+//   }
+//   return 0;
+// }
+
+/*
+     Qs: Reverse a given number and print the result
+  */
+
+// int main()
+// {
+//   int n = 123456;
+//   int res = 0;
+
+//   while (n > 0)
+//   {
+//     int lastDig = n % 10;
+//     res = res * 10 + lastDig;
+//     n /= 10;
+//   }
+//   cout << "Result is: " << res;
+//   return 0;
+// }
