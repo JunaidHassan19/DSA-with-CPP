76d5128da28bf20f8417e6d5f301e26e01213440
JunaidHassan19 <thejunni01@gmail.com>
2026-02-16 10:53:05 +0530

    More Practice

--- 05 Patterns/PracticeQs.cpp
+++ 05 Patterns/PracticeQs.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 // Pattern 1
+/*
 int main()
 {
+  // int n = 4;
+  // for (int i = 1; i <= n; i++)
+  // {
+  //   for (int j = 1; j <= n; j++)
+  //   {
+  //     cout << i << " ";
+  //   }
+  //   cout << endl;
+  // }
+
   int n = 4;
   for (int i = 1; i <= n; i++)
   {
@@ -15,3 +26,4 @@ int main()
   }
   return 0;
 }
+*/
