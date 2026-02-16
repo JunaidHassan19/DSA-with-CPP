f6ff2ccd40e7bad7c3a4f80ff1982d3c0b59ee45
JunaidHassan19 <thejunni01@gmail.com>
2026-02-16 11:36:50 +0530

    Pattern 7

--- 05 Patterns/PracticeQs.cpp
+++ 05 Patterns/PracticeQs.cpp
@@ -162,3 +162,23 @@ int main()
   return 0;
 }
 */
+
+// Pattern 7
+int main()
+{
+  int n = 4;
+  for (int i = 1; i <= n; i++)
+  {
+    // Space
+    for (int j = 1; j <= n - i; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
