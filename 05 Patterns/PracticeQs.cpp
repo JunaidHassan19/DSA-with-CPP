5d01ab9c30794e710b6895e777d093cbc9fc2732
JunaidHassan19 <thejunni01@gmail.com>
2026-02-16 11:55:02 +0530

    Pattern 9

--- 05 Patterns/PracticeQs.cpp
+++ 05 Patterns/PracticeQs.cpp
@@ -230,3 +230,81 @@ int main()
   return 0;
 }
 */
+
+// Pattern 9
+/*
+int main()
+{
+  // int n = 5;
+  // for (int i = 1; i <= n; i++)
+  // {
+  //   // Space
+  //   for (int j = 1; j <= n - i; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   for (int j = 1; j <= 2 * i - 1; j++)
+  //   {
+  //     cout << "*";
+  //   }
+  //   for (int j = 1; j <= n - i; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   cout << endl;
+  // }
+  // for (int i = n; i >= 1; i--)
+  // {
+  //   // Space
+  //   for (int j = 1; j <= n - i; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   for (int j = 1; j <= 2 * i - 1; j++)
+  //   {
+  //     cout << "*";
+  //   }
+  //   for (int j = 1; j <= n - i; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  int n = 6;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n - i; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= 2 * i - 1; j++)
+    {
+      cout << "*";
+    }
+    for (int j = 1; j <= n - i; j++)
+    {
+      cout << " ";
+    }
+    cout << endl;
+  }
+  for (int i = n; i >= 1; i--)
+  {
+    for (int j = 1; j <= n - i; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= 2 * i - 1; j++)
+    {
+      cout << "*";
+    }
+    for (int j = 1; j <= n - i; j++)
+    {
+      cout << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
+*/
