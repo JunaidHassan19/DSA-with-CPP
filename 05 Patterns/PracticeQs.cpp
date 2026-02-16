6306baae4c79c637a10e9ea9b7b44095784d7ece
JunaidHassan19 <thejunni01@gmail.com>
2026-02-16 10:56:54 +0530

    Practice

--- 05 Patterns/PracticeQs.cpp
+++ 05 Patterns/PracticeQs.cpp
@@ -27,3 +27,20 @@ int main()
   return 0;
 }
 */
+
+// Pattern 2
+/*
+int main()
+{
+  int n = 4;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
+*/
