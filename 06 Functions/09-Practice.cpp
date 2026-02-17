04defebbdf860a41f9aca17b0380f5828328669c
JunaidHassan19 <thejunni01@gmail.com>
2026-02-17 11:54:49 +0530

    Fine the binomial coefficient for given n & r

--- 06 Functions/09-Practice.cpp
+++ 06 Functions/09-Practice.cpp
@@ -38,3 +38,35 @@ Logic:
 output:
 6
 */
+
+/*
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+  int fact = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    fact *= i;
+  }
+  return fact;
+}
+
+int binoCoeff(int n, int r)
+{
+  int val1 = factorial(n);
+  int val2 = factorial(r);
+  int val3 = factorial(n - r);
+
+  int result = val1 / (val2 * val3);
+  return result;
+}
+
+int main()
+{
+  cout << binoCoeff(4, 2);
+  return 0;
+}
+*/
