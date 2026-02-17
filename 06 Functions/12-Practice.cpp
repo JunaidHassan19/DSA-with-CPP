f00b36d0ac1ae37d4bcd1468782e456c49980749
JunaidHassan19 <thejunni01@gmail.com>
2026-02-17 22:44:24 +0530

    Mooore Practice

--- 06 Functions/12-Practice.cpp
+++ 06 Functions/12-Practice.cpp
@@ -212,3 +212,40 @@ int main()
   cout << sum(5.2, 2.8) << endl;
 }
 */
+
+bool isPrime(int n)
+{
+  if (n == 1)
+  {
+    return false;
+  }
+  for (int i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+void allPrimes(int n)
+{
+  for (int i = 2; i <= n; i++)
+  {
+    if (isPrime(i))
+    {
+      cout << i << " ";
+    }
+  }
+  cout << endl;
+}
+
+int main()
+{
+  int n;
+  cout << "Enter a numbe: ";
+  cin >> n;
+
+  allPrimes(n);
+}
