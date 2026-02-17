0715bc97c15cac575434ffc56c2056f527cd3f31
JunaidHassan19 <thejunni01@gmail.com>
2026-02-17 21:44:37 +0530

    Practice

---
+++ 06 Functions/12-Practice.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// 01 topic
+/*
+void sayHello()
+{
+  cout << "Hello :)";
+}
+
+void hello2()
+{
+  sayHello();
+  cout << "I'm hello2";
+}
+
+int main()
+{
+  hello2();
+  return 0;
+}
+*/
+
+// 02 topic
+/*
+void sayHello();
+
+int main()
+{
+  sayHello();
+  return 0;
+}
+void sayHello()
+{
+  cout << "Helo :)";
+}
+*/
+
+// 03 topic
