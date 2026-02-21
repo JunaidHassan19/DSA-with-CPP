3a811c50f6e07cd014a6121a869559ed6c78697b
JunaidHassan19 <thejunni01@gmail.com>
2026-02-21 19:59:51 +0530

    Practice

---
+++ Practice.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Creating an Array
+/*
+int main()
+{
+  int arr[5] = {1, 2, 3, 4, 5};
+  int arr[5] = {1, 2};
+  int arr[] = {1, 2, 3, 4, 5};
+  int arr[5];
+
+  cout << arr[0] << endl;
+  cout << arr[1] << endl;
+  cout << arr[2] << endl;
+  cout << arr[4] << endl;
+
+  return 0;
+}
+*/
