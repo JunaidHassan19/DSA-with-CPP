b3179952cc7df0c2dcc9e0f9eb7beb4e23ec89a3
JunaidHassan19 <thejunni01@gmail.com>
2026-02-22 22:15:58 +0530

    Max Sum Subarray (Brute Force Approach)

---
+++ 09 Arrays (Part ll)/02-MaxSumSubarray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void maxSubarraySum(int *arr, int len)
+{
+  int maxSum = INT_MIN;
+  for (int start = 0; start < len; start++)
+  {
+    for (int end = start; end < len; end++)
+    {
+      int currSum = 0;
+      for (int i = start; i <= end; i++)
+      {
+        currSum += arr[i];
+      }
+      cout << currSum << ",";
+      maxSum = max(maxSum, currSum);
+    }
+    cout << endl;
+  }
+  cout << "Maxinum Subarray sum = " << maxSum;
+}
+
+int main()
+{
+  int arr[6] = {2, -3, 6, -5, 4, 2};
+  int len = sizeof(arr) / sizeof(int);
+
+  maxSubarraySum(arr, len);
+  return 0;
+}
