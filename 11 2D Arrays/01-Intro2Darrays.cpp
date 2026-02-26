ae7dd2fcd18475ea24da3f8e4c41c74fbfd0c61f
JunaidHassan19 <thejunni01@gmail.com>
2026-02-26 20:59:02 +0530

    Introduction to 2D Arrays

--- 11 2D Arrays/01-Intro2Darrays.cpp
+++ 11 2D Arrays/01-Intro2Darrays.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+// Definition of 2D Arrays:
+// A 2D array is a collection of elements arranged in rows and columns.
+// Syntax: dataType arrayName[rows][columns];
+// Example: int arr[3][3] means 3 rows and 3 columns
+// Elements are stored in row-major order (row by row in memory)
+// Access elements using: arrayName[rowIndex][columnIndex]
 
 int main()
 {
@@ -7,9 +13,9 @@ int main()
                         {88, 85, 89},
                         {94, 86, 87}};
 
-  cout << students[1][1] << endl;
-  cout << students[0][1] << endl;
-  cout << students[2][2] << endl;
+  cout << students[1][1] << endl; // 85
+  cout << students[0][1] << endl; // 97
+  cout << students[2][2] << endl; // 87
 
   return 0;
 }
