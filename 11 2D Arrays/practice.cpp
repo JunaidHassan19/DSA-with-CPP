93f3eeba0db06748a206035a9cf36f7b34c6c864
JunaidHassan19 <thejunni01@gmail.com>
2026-02-27 22:16:14 +0530

    Practice -Spiral Matrix

--- 11 2D Arrays/practice.cpp
+++ 11 2D Arrays/practice.cpp
@@ -89,6 +89,120 @@ int main()
   */
 
 // Spiral Matrix
+/*
+// void spiralMat(int mat[][4], int n, int m)
+// {
+//   int srow = 0, scol = 0;
+//   int erow = n - 1, ecol = m - 1;
+//   while (srow <= erow && scol <= ecol)
+//   {
+//     // Top
+//     for (int j = scol; j <= ecol; j++)
+//     {
+//       cout << mat[srow][j] << " ";
+//     }
+//     // Right
+//     for (int i = srow + 1; i <= erow; i++)
+//     {
+//       cout << mat[i][ecol] << " ";
+//     }
+//     // bottom
+//     for (int j = ecol - 1; j >= scol; j--)
+//     {
+//       if (srow == erow)
+//       {
+//         break;
+//       }
+//       cout << mat[erow][j] << " ";
+//     }
+//     // left
+//     for (int i = erow - 1; i >= srow + 1; i--)
+//     {
+//       if (scol == ecol)
+//       {
+//         break;
+//       }
+//       cout << mat[i][scol] << " ";
+//     }
+//     srow++;
+//     scol++;
+//     erow--;
+//     ecol--;
+//   }
+//   cout << endl;
+// }
+
+// void spiralMat(int mat[][4], int n, int m)
+// {
+//   int srow = 0, scol = 0;
+//   int erow = n - 1, ecol = m - 1;
+//   while (srow <= erow && scol <= ecol)
+//   {
+//     // top
+//     for (int j = scol; j <= ecol; j++)
+//     {
+//       cout << mat[srow][j] << " ";
+//     }
+//     // right
+//     for (int i = srow + 1; i <= erow; i++)
+//     {
+//       cout << mat[i][ecol] << " ";
+//     }
+//     // bottom
+//     for (int j = ecol - 1; j >= scol; j--)
+//     {
+//       cout << mat[erow][j] << " ";
+//     }
+//     // left
+//     for (int i = erow - 1; i >= srow + 1; i--)
+//     {
+//       cout << mat[i][scol] << " ";
+//     }
+//     srow++;
+//     scol++;
+//     erow--;
+//     ecol--;
+//   }
+// }
+
+// void spiralMat(int mat[][4], int n, int m)
+// {
+//   int srow = 0, scol = 0;
+//   int erow = n - 1, ecol = m - 1;
+//   while (srow <= erow && scol <= ecol)
+//   {
+//     // Top
+//     for (int i = scol; i <= ecol; i++)
+//     {
+//       cout << mat[srow][i] << " ";
+//     }
+//     // right
+//     for (int j = srow + 1; j <= erow; j++)
+//     {
+//       cout << mat[j][ecol] << " ";
+//     }
+//     // bottom
+//     for (int i = ecol - 1; i >= srow; i--)
+//     {
+//       if(srow == erow) {
+//         break;
+//       }
+//       cout << mat[erow][i] << " ";
+//     }
+//     // left
+//     for (int j = erow - 1; j >= srow + 1; j--)
+//     {
+//       if(scol == ecol) {
+//         break;
+//       }
+//       cout << mat[j][scol] << " ";
+//     }
+//     srow++;
+//     scol++;
+//     erow--;
+//     ecol--;
+//   }
+// }
 
 void spiralMat(int mat[][4], int n, int m)
 {
@@ -97,40 +211,32 @@ void spiralMat(int mat[][4], int n, int m)
   while (srow <= erow && scol <= ecol)
   {
     // Top
-    for (int j = scol; j <= ecol; j++)
+    for (int i = scol; i <= ecol; i++)
     {
-      cout << mat[srow][j] << " ";
+      cout << mat[srow][i] << " ";
     }
     // Right
-    for (int i = srow + 1; i <= erow; i++)
+    for (int j = srow + 1; j <= erow; j++)
     {
-      cout << mat[i][ecol] << " ";
+      cout << mat[j][ecol] << " ";
     }
-    // bottom
-    for (int j = ecol - 1; j >= scol; j--)
+    // Bottom
+    for (int i = ecol - 1; i >= scol; i--)
     {
-      if (srow == erow)
-      {
-        break;
-      }
-      cout << mat[erow][j] << " ";
+      cout << mat[erow][i] << " ";
     }
     // left
-    for (int i = erow - 1; i >= srow + 1; i--)
+    for (int j = erow - 1; j >= srow + 1; j--)
     {
-      if (scol == ecol)
-      {
-        break;
-      }
-      cout << mat[i][scol] << " ";
+      cout << mat[j][scol] << " ";
     }
     srow++;
     scol++;
     erow--;
     ecol--;
   }
-  cout << endl;
 }
+
 int main()
 {
   int arr[4][4] = {{1, 2, 3, 4},
@@ -140,6 +246,7 @@ int main()
   spiralMat(arr, 4, 4);
   return 0;
 }
+  */
 
 // Diagonal Sum
 /*
