7acb4b2cad928ea1cb6cff23b6f859172724740e
JunaidHassan19 <thejunni01@gmail.com>
2026-02-28 11:49:58 +0530

    Practice -Search Matrix -Brute force

--- 11 2D Arrays/practice.cpp
+++ 11 2D Arrays/practice.cpp
@@ -306,3 +306,32 @@ int main()
   return 0;
 }
   */
+
+// Search Matrix - Brute force
+
+void searchMatrix(int mat[][4], int n, int m, int key)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (mat[i][j] == key)
+      {
+        cout << "Found at cell (" << i << "," << j << ")" << endl;
+        return;
+      }
+    }
+  }
+  cout << "Key not found" << endl;
+}
+
+int main()
+{
+  int matrix[4][4] = {{10, 20, 30, 40},
+                      {15, 25, 35, 45},
+                      {27, 29, 37, 48},
+                      {32, 33, 39, 50}};
+
+  searchMatrix(matrix, 4, 4, 37);
+  return 0;
+}
