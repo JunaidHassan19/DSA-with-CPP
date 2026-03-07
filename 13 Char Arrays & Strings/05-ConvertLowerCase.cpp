e22b25210505a232117c6259a87ec6bfa6cfda45
JunaidHassan19 <thejunni01@gmail.com>
2026-03-07 22:49:31 +0530

    Convert to LowerCase

---
+++ 13 Char Arrays & Strings/05-ConvertLowerCase.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void toLower(char word[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    char ch = word[i];
+    if (ch >= 'a' && ch <= 'z')
+    {
+      continue;
+    }
+    else
+    {
+      word[i] = ch - 'A' + 'a';
+    }
+  }
+}
+
+int main()
+{
+  char word[] = "cODe";
+
+  toLower(word, strlen(word));
+  cout << word;
+
+  return 0;
+}
