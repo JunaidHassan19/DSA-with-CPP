20a5fe21ed89203c0cf80e03ba34d03ee14bb960
JunaidHassan19 <thejunni01@gmail.com>
2026-03-07 21:31:20 +0530

    Character Array

--- 13 Char Arrays & Strings/02-CharArray.cpp
+++ 13 Char Arrays & Strings/02-CharArray.cpp
@@ -17,18 +17,29 @@ int main()
   cout << arr << endl;
 
   // Creation & Output.
-  char work[] = "code";
+  char work[] = "code"; // null character is automatically added at the end of the string
   cout << work << endl;
+  cout << strlen(work) << endl; // prints the length of the string "code", which is 4
 
-  char work1[5] = "code";
+  char work1[5] = "code"; // null character is automatically added at the end of the string, but it exceeds the array size, which can lead to undefined behavior
   cout << work1 << endl;
 
-  char work2[] = {'c', 'o', 'd', 'e', '\0'};
+  char work2[] = {'c', 'o', 'd', 'e', '\0'}; // null character is explicitly added at the end of the string
   cout << work2 << endl;
 
-  char work3[50] = {'c', 'o', 'd', 'e', '\0'};
+  char work3[50] = {'c', 'o', 'd', 'e', '\0'}; // null character is explicitly added at the end of the string, and the array size is sufficient to hold the string
   cout << work3 << endl;
-  cout << strlen(work) << endl;
 
   return 0;
 }
+
+/*
+definition of char array:
+- A char array is a collection of characters stored in contiguous memory locations.
+- It is used to store strings in C++.
+- A char array can be initialized using an initializer list (e.g., char arr[] = {'a', 'b', 'c', '\0'};) or using a string literal (e.g., char arr[] = "abc";).
+- When a char array is initialized with a string literal, a null character ('\0') is automatically added at the end of the string to indicate the end of the string.
+- The size of a char array can be specified explicitly (e.g., char arr[10] = "hello";) or can be determined by the compiler based on the initializer (e.g., char arr[] = "hello";).
+- It is important to ensure that the char array has enough space to hold the string and the null character to avoid undefined behavior.
+
+*/
