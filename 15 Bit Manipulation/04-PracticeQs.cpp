eca98015c37c946c787bb6c5a070f5b2082f7eb4
JunaidHassan19 <thejunni01@gmail.com>
2026-03-10 16:00:03 +0530

    Practice Qs

---
+++ 15 Bit Manipulation/04-PracticeQs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Qs. Predict the output for ~4.
+  cout << "~4 = " << (~4) << endl;
+
+  // Qs. Predict the output for 8 >> 1.
+  cout << "8 >> 1 = " << (8 >> 1) << endl;
+  return 0;
+}
