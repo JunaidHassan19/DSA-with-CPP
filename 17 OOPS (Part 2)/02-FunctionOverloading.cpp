f1bbf2cb9d856fe26494def7adbdc34e393e9c72
JunaidHassan19 <thejunni01@gmail.com>
2026-03-12 20:36:36 +0530

    Function Overloading -Compile Time Polymorphism

---
+++ 17 OOPS (Part 2)/02-FunctionOverloading.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class print
+{
+public:
+  void show(int x)
+  {
+    cout << "Value of x is: " << x << endl;
+  }
+
+  void show(string n)
+  {
+    cout << "Hey " << n << endl;
+  }
+};
+
+int main()
+{
+  print pt;
+  pt.show(20);
+  pt.show("junni");
+  return 0;
+}
+
+/*
+Function Overloading: Same function name but different parameters (type or number)
+- The compiler determines which function to call based on the arguments passed.
+- This allows us to use the same function name for different purposes, improving code readability and organization.
+Example:
+void show(int x) { ... } // for integers
+void show(string n) { ... } // for strings
+In main():
+pt.show(20); // calls show(int)
+pt.show("junni"); // calls show(string)
+*/
