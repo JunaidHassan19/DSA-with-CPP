3f6ec6d0f0b6a9424e32e9caa16a0f818057f300
JunaidHassan19 <thejunni01@gmail.com>
2026-03-12 12:50:44 +0530

    Inheritance Basic

---
+++ 16 OOPS (Part 1)/11-InheritanceBasic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+public:
+  string color;
+
+  void eat()
+  {
+    cout << "Eats.." << endl;
+  }
+
+  void breath()
+  {
+    cout << "Breathes.." << endl;
+  }
+};
+
+class cat : public Animal
+{
+  int legs;
+
+  void walk()
+  {
+    cout << "Walking.." << endl;
+  }
+};
