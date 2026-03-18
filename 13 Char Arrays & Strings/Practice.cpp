6fafa227a7494499aadc58930a2d56cbfcb863ba
JunaidHassan19 <thejunni01@gmail.com>
2026-03-18 21:35:09 +0530

    reverseVowels practice

--- 13 Char Arrays & Strings/Practice.cpp
+++ 13 Char Arrays & Strings/Practice.cpp
@@ -136,11 +136,61 @@ int main()
 //   }
 // }
 
+// reverse only the vowels
+
+// void reverseVowels(char word[], int n)
+// {
+//   int st = 0;
+//   int end = n - 1;
+//   while (st < end)
+//   {
+//     while (st < end && word[st] != 'a' && word[st] != 'e' && word[st] != 'i' && word[st] != 'o' && word[st] != 'u')
+//     {
+//       st++;
+//     }
+//     while (st < end && word[end] != 'a' && word[end] != 'e' && word[end] != 'i' && word[end] != 'o' && word[end] != 'u')
+//     {
+//       end--;
+//     }
+//     if (st < end)
+//     {
+//       swap(word[st], word[end]);
+//       st++;
+//       end--;
+//     }
+//   }
+// }
+
+void reverseVowels(char word[], int n)
+{
+  int st = 0;
+  int end = n - 1;
+
+  while (st < end)
+  {
+    while (st < end && word[st] != 'a' && word[st] != 'e' && word[st] != 'i' && word[st] != 'o' && word[st] != 'u')
+    {
+      st++;
+    }
+    while (st < end && word[end] != 'a' && word[end] != 'e' && word[end] != 'i' && word[end] != 'o' && word[end] != 'u')
+    {
+      end--;
+    }
+    if (st < end)
+    {
+      swap(word[st], word[end]);
+      st++;
+      end--;
+    }
+  }
+}
+
 int main()
 {
-  char arr[] = "reverse";
-  // reverse(arr, strlen(arr));
-  cout << arr << endl;
+  char word[] = "abcdef";
+  // reverse(word, strlen(arr));
+  reverseVowels(word, strlen(word));
+  cout << word << endl;
 
   return 0;
 }
