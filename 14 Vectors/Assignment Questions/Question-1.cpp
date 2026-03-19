005b7594f70e7f7fbebff4c0bdb99f6b2feedd8a
JunaidHassan19 <thejunni01@gmail.com>
2026-03-19 22:25:21 +0530

    findErrorNums -practice

--- 14 Vectors/Assignment Questions/Question-1.cpp
+++ 14 Vectors/Assignment Questions/Question-1.cpp
@@ -28,6 +28,30 @@ using namespace std;
 //   return ans;
 // }
 
+// vector<int> findErrorNums(vector<int> &nums)
+// {
+//   vector<int> ans;
+//   int n = nums.size();
+//   vector<bool> isPresent(n + 1, false);
+//   for (int i = 0; i < n; i++)
+//   {
+//     if (isPresent[nums[i]])
+//     {
+//       ans.push_back(nums[i]);
+//     }
+//     isPresent[nums[i]] = true;
+//   }
+//   for (int i = 1; i < isPresent.size(); i++)
+//   {
+//     if (!isPresent[i])
+//     {
+//       ans.push_back(i);
+//       break;
+//     }
+//   }
+//   return ans;
+// }
+
 vector<int> findErrorNums(vector<int> &nums)
 {
   vector<int> ans;
