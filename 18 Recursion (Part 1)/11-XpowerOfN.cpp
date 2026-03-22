adeeab7eacaf30345070bdaad1fe02e06dddf186
JunaidHassan19 <thejunni01@gmail.com>
2026-03-22 22:23:22 +0530

    Print X to the Power of N

--- 18 Recursion (Part 1)/11-XpowerOfN.cpp
+++ 18 Recursion (Part 1)/11-XpowerOfN.cpp
@@ -25,6 +25,26 @@ int main()
   return 0;
 }
 
+// int pow(int x, int n)
+// {
+//   if (n == 0) // base case: any number raised to the power of 0 is 1
+//   {
+//     return 1;
+//   }
+//   int halfPow = pow(x, n / 2);           // recursive case: calculate the half power
+//   int halfPowSquare = halfPow * halfPow; // square the half power to get the full power for even n
+//   if (n % 2 != 0) // if n is odd, we need to multiply by x one more time to account for the odd exponent
+//   {
+//     return x * halfPowSquare; // return x multiplied by the squared half power for odd n
+//   }
+//   return halfPowSquare; // return the squared half power for even n
+// }
+// int main()
+// {
+//   cout << pow(2, 5);
+//   return 0;
+// }
+
 /*
 
 logic:
