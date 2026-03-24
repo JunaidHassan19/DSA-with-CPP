442686ff1c78435ffc00b443441b63199784dfe4
JunaidHassan19 <thejunni01@gmail.com>
2026-03-24 14:57:31 +0530

    Merge sort Practice

--- 20 Divide & Conquer/Practice.cpp
+++ 20 Divide & Conquer/Practice.cpp
@@ -4,6 +4,56 @@ using namespace std;
 
 // Merge sort Practice
 
+// void merge(int arr[], int si, int mid, int ei)
+// {
+//   vector<int> temp;
+//   int i = si;
+//   int j = mid + 1;
+//   while (i <= mid && j <= ei)
+//   {
+//     if (arr[i] <= arr[j])
+//     {
+//       temp.push_back(arr[i++]);
+//     }
+//     else
+//     {
+//       temp.push_back(arr[j++]);
+//     }
+//   }
+//   while (i <= mid)
+//   {
+//     temp.push_back(arr[i++]);
+//   }
+//   while (j <= ei)
+//   {
+//     temp.push_back(arr[j++]);
+//   }
+//   // copy to original
+//   for (int idx = si, x = 0; idx <= ei; idx++)
+//   {
+//     arr[idx] = temp[x++];
+//   }
+// }
+// void mergeSort(int arr[], int si, int ei)
+// {
+//   if (si >= ei)
+//   {
+//     return;
+//   }
+//   int mid = (si + ei) / 2;
+//   mergeSort(arr, si, mid);     // left
+//   mergeSort(arr, mid + 1, ei); // right
+//   merge(arr, si, mid, ei);
+// }
+// void printArray(int arr[], int n)
+// {
+//   for (int i = 0; i < n; i++)
+//   {
+//     cout << arr[i] << " ";
+//   }
+//   cout << endl;
+// }
+
 void merge(int arr[], int si, int mid, int ei)
 {
   vector<int> temp;
@@ -30,7 +80,8 @@ void merge(int arr[], int si, int mid, int ei)
     temp.push_back(arr[j++]);
   }
 
-  // copy to original
+  // to orinigla
+
   for (int idx = si, x = 0; idx <= ei; idx++)
   {
     arr[idx] = temp[x++];
@@ -43,11 +94,9 @@ void mergeSort(int arr[], int si, int ei)
   {
     return;
   }
-
   int mid = (si + ei) / 2;
   mergeSort(arr, si, mid);     // left
   mergeSort(arr, mid + 1, ei); // right
-
   merge(arr, si, mid, ei);
 }
 
