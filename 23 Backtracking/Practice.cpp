7ef88560894089b5ccb4f6a155405e0646195ac0
JunaidHassan19 <thejunni01@gmail.com>
2026-03-30 18:51:15 +0530

    Practice Find Permutations

--- 23 Backtracking/Practice.cpp
+++ 23 Backtracking/Practice.cpp
@@ -112,6 +112,22 @@ int main()
 //   }
 // }
 
+// void permutations(string str, string ans)
+// {
+//   int n = str.size();
+//   if (n == 0)
+//   {
+//     cout << ans << endl;
+//     return;
+//   }
+//   for (int i = 0; i < n; i++)
+//   {
+//     char ch = str[i];
+//     string nextStr = str.substr(0, i) + str.substr(i + 1, n - i - 1);
+//     permutations(nextStr, ans + ch);
+//   }
+// }
+
 void permutations(string str, string ans)
 {
   int n = str.size();
