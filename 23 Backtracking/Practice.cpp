dc60f9f5a1769eca7f9b3d7e806bcd6a8f8ef35d
JunaidHassan19 <thejunni01@gmail.com>
2026-03-31 20:26:09 +0530

    Practice 2X2 N-Queens Problem

--- 23 Backtracking/Practice.cpp
+++ 23 Backtracking/Practice.cpp
@@ -202,6 +202,51 @@ int main()
 //   return 0;
 // }
 
+// void printBoard(vector<vector<char>> board)
+// {
+//   int n = 2;
+//   for (int i = 0; i < n; i++)
+//   {
+//     for (int j = 0; j < n; j++)
+//     {
+//       cout << board[i][j] << " ";
+//     }
+//     cout << endl;
+//   }
+//   cout << "-----" << endl;
+// }
+// void nQueens(vector<vector<char>> board, int row)
+// {
+//   int n = board.size();
+//   if (row == n)
+//   {
+//     printBoard(board);
+//     return;
+//   }
+//   for (int j = 0; j < n; j++)
+//   {
+//     board[row][j] = 'Q';
+//     nQueens(board, row + 1);
+//     board[row][j] = '.';
+//   }
+// }
+// int main()
+// {
+//   vector<vector<char>> board;
+//   int n = 2;
+//   for (int i = 0; i < n; i++)
+//   {
+//     vector<char> newRow;
+//     for (int j = 0; j < n; j++)
+//     {
+//       newRow.push_back('.');
+//     }
+//     board.push_back(newRow);
+//   }
+//   nQueens(board, 0);
+//   return 0;
+// }
+
 void printBoard(vector<vector<char>> board)
 {
   int n = 2;
@@ -209,7 +254,7 @@ void printBoard(vector<vector<char>> board)
   {
     for (int j = 0; j < n; j++)
     {
-      cout << board[i][j] << " ";
+      cout << board[j][i] << " ";
     }
     cout << endl;
   }
@@ -236,7 +281,6 @@ int main()
 {
   vector<vector<char>> board;
   int n = 2;
-
   for (int i = 0; i < n; i++)
   {
     vector<char> newRow;
@@ -246,7 +290,6 @@ int main()
     }
     board.push_back(newRow);
   }
-
   nQueens(board, 0);
   return 0;
 }
