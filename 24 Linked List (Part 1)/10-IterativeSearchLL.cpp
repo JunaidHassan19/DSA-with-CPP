e5ff3b4e2c50e214f188aac60d88d03b9b1b26f5
JunaidHassan19 <thejunni01@gmail.com>
2026-04-03 19:46:10 +0530

    Iterative search in LL

---
+++ 24 Linked List (Part 1)/10-IterativeSearchLL.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+using namespace std;
+
+// Print Linked List
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+  Node(int val)
+  {
+    data = val;
+    next = NULL;
+  }
+
+  ~Node()
+  {
+    if (next != NULL)
+    {
+      delete next;
+      next = NULL;
+    }
+  }
+};
+
+class List
+{
+  Node *head;
+  Node *tail;
+
+public:
+  List()
+  {
+    head = NULL;
+    tail = NULL;
+  }
+
+  ~List()
+  {
+    if (head != NULL)
+    {
+      delete head;
+      head = NULL;
+    }
+  }
+
+  // push_front
+  void push_front(int val)
+  {
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+      head = tail = newNode;
+    }
+    else
+    {
+      newNode->next = head;
+      head = newNode;
+    }
+  }
+
+  // push_back
+  void push_back(int val)
+  {
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+      head = tail = newNode;
+    }
+    else
+    {
+      tail->next = newNode;
+      tail = newNode;
+    }
+  };
+
+  // Print ll
+  void printList()
+  {
+    Node *temp = head;
+    while (temp != NULL)
+    {
+      cout << temp->data << "->";
+      temp = temp->next;
+    }
+    cout << "NULL" << endl;
+  }
+
+  // Insert Middle in LL
+  void insert(int val, int pos)
+  {
+    Node *newNode = new Node(val);
+
+    Node *temp = head;
+    for (int i = 0; i < pos - 1; i++)
+    {
+      temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+  }
+
+  // Pop Front in LL
+  void pop_front()
+  {
+    if (head == NULL)
+    {
+      cout << "LL is empty";
+      return;
+    }
+    Node *temp = head;
+    head = head->next;
+    temp->next = NULL;
+    delete temp;
+  }
+
+  // Pop Back in LL
+  void pop_back()
+  {
+    Node *temp = head;
+
+    while (temp->next->next != NULL)
+    {
+      temp = temp->next;
+    }
+    temp->next = NULL;
+    delete tail;
+    tail = temp;
+  }
+
+  // Iterative search in LL
+  int searchItr(int key)
+  {
+    Node *temp = head;
+    int idx = 0;
+    while (temp != NULL)
+    {
+      if (temp->data == key)
+      {
+        return idx;
+      }
+      temp = temp->next;
+      idx++;
+    }
+    return -1;
+  }
+};
+
+int main()
+{
+  List ll;
+  ll.push_front(4);
+  ll.push_front(3);
+  ll.push_front(2);
+  ll.push_front(1);
+  ll.printList();
+
+  // ll.insert(100, 4);
+
+  // ll.pop_front();
+  // ll.printList();
+
+  // ll.pop_back();
+  // ll.printList();
+
+  cout << "KEY = " << ll.searchItr(3) << endl;
+  return 0;
+}
