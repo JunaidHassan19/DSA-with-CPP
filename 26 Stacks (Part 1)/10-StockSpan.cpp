dff1523ce68d2cf37c8d411b32b5bf684f543221
JunaidHassan19 <thejunni01@gmail.com>
2026-04-08 00:02:58 +0530

    Stock Span Problem

---
+++ 26 Stacks (Part 1)/10-StockSpan.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Stock Span Problem
+void stockSpamProblem(vector<int> stock, vector<int> span)
+{
+  stack<int> s;
+  s.push(0);
+  span[0] = 1;
+
+  for (int i = 1; i < stock.size(); i++)
+  {
+    int currPrice = stock[i];
+    while (!s.empty() && currPrice >= stock[s.top()])
+    {
+      s.pop();
+    }
+
+    if (s.empty())
+    {
+      span[i] = i + 1;
+    }
+    else
+    {
+      int prevHigh = s.top();
+      span[i] = i - prevHigh;
+    }
+  }
+
+  for (int i = 0; i < span.size(); i++)
+  {
+    cout << span[i] << " ";
+  }
+  cout << endl;
+}
+
+int main()
+{
+  vector<int> stock = {100, 80, 60, 70, 60, 85, 1000};
+  vector<int> span = {0, 0, 0, 0, 0, 0, 0};
+
+  stockSpamProblem(stock, span);
+  return 0;
+}
