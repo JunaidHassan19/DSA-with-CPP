e6a75d1a73453cfefbab31ea16e5e6a2dcd3043b
JunaidHassan19 <thejunni01@gmail.com>
2026-04-07 18:21:50 +0530

    Practice - Stack using vector and class template

--- 26 Stacks (Part 1)/Practice.cpp
+++ 26 Stacks (Part 1)/Practice.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Practice - Stack using vector
-
+/*
 class Stack
 {
   vector<int> vec;
@@ -54,5 +54,64 @@ int main()
     cout << s.top() << " ";
     s.pop();
   }
+  return 0;
+}
+*/
+
+// Practice - Stack using vector and class template
+template <class T>
+class Stack
+{
+  vector<T> vec;
+
+public:
+  void push(T val)
+  {
+    vec.push_back(val);
+  }
+
+  void pop()
+  {
+    if (isEmpty())
+    {
+      cout << "Stack is Empty" << endl;
+      return;
+    }
+    vec.pop_back();
+  }
+
+  T top()
+  {
+    if (isEmpty())
+    {
+      cout << "Stack is Empty" << endl;
+      return -1;
+    }
+    int lastIdx = vec.size() - 1;
+    return vec[lastIdx];
+  }
+
+  bool isEmpty()
+  {
+    return vec.size() == 0;
+  }
+};
+
+int main()
+{
+  // Stack<int> s;
+  // Stack<float> s;
+  Stack<char> s;
+
+  s.push('J');
+  s.push('S');
+  s.push('H');
+
+  while (!s.isEmpty())
+  {
+    cout << s.top() << " ";
+    s.pop();
+  }
+
   return 0;
 }
