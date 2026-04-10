0205b4a9002231dc357d911ab604728be5331ad7
JunaidHassan19 <thejunni01@gmail.com>
2026-04-10 23:29:00 +0530

    Practice - Duplicate Parentheses

--- 27 Stacks (Part 2)/practice.cpp
+++ 27 Stacks (Part 2)/practice.cpp
@@ -98,6 +98,33 @@ int main()
 */
 
 // Practice - Duplicate Parentheses
+
+// bool isDuplicate(string str)
+// {
+//   stack<char> s;
+//   for (int i = 0; i < str.size(); i++)
+//   {
+//     char ch = str[i];
+//     if (ch != ')')
+//     {
+//       s.push(ch);
+//     }
+//     else
+//     {
+//       if (s.top() == '(')
+//       {
+//         return true;
+//       }
+//       while (s.top() != '(')
+//       {
+//         s.pop();
+//       }
+//       s.pop();
+//     }
+//   }
+//   return false;
+// }
+
 bool isDuplicate(string str)
 {
   stack<char> s;
