5db51af68c79ac779d4df741863164ea73f87b93
JunaidHassan19 <thejunni01@gmail.com>
2026-04-16 20:26:43 +0530

    Practice Pair in C++

--- 29 Greedy Algorithms/Practice.cpp
+++ 29 Greedy Algorithms/Practice.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 // Practice Problem: Activity Selection Problem
+/*
 int maxActivities(vector<int> start, vector<int> end)
 {
   int count = 1;
@@ -24,5 +26,30 @@ int main()
   vector<int> end = {2, 4, 6, 7, 9, 9};
 
   cout << maxActivities(start, end) << endl;
+  return 0;
+}
+*/
+
+// Practice Pair in C++
+
+bool complare(pair<int, int> p1, pair<int, int> p2)
+{
+  return p1.second > p2.second;
+}
+
+int main()
+{
+  vector<pair<int, int>> act(3, make_pair(0, 0));
+
+  act[0] = make_pair(0, 9);
+  act[1] = make_pair(1, 2);
+  act[2] = make_pair(2, 4);
+
+  for (int i = 0; i < act.size(); i++)
+  {
+    cout << "A" << i << " : " << act[i].first << ", " << act[i].second << endl;
+  }
+
+  sort(act.begin(), act.end(), complare);
   return 0;
 }
