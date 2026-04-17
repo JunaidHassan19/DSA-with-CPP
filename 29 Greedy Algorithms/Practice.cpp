0f566db2d162bc8e36b49ce68b02ab0dd0097b07
JunaidHassan19 <thejunni01@gmail.com>
2026-04-17 23:33:12 +0530

    Practice Job Sequencing Problem

--- 29 Greedy Algorithms/Practice.cpp
+++ 29 Greedy Algorithms/Practice.cpp
@@ -150,7 +150,7 @@ int main()
 */
 
 // Practice Indian Coins Problem
-
+/*
 int getMinChange(vector<int> coins, int V)
 {
   int ans = 0;
@@ -176,5 +176,45 @@ int main()
 
   getMinChange(coins, V);
 
+  return 0;
+}
+*/
+
+// Practice Job Sequencing Problem
+bool compare(pair<int, int> p1, pair<int, int> p2)
+{
+  return p1.second > p2.second; // Sort in descending order based on profit
+}
+
+int maxProfit(vector<pair<int, int>> jobs)
+{
+  sort(jobs.begin(), jobs.end(), compare);
+
+  int profit = jobs[0].second;
+  int safeDedline = 2;
+
+  for (int i = 1; i < jobs.size(); i++)
+  {
+    if (jobs[i].first >= safeDedline)
+    {
+      profit += jobs[i].second;
+      safeDedline++;
+    }
+  }
+  cout << "Maximum profit = " << profit << endl;
+  return profit;
+}
+
+int main()
+{
+  int n = 4;
+  vector<pair<int, int>> jobs(n, make_pair(0, 0));
+
+  jobs[0] = make_pair(4, 20); // Job 1: deadline = 4, profit = 20
+  jobs[1] = make_pair(1, 10); // Job 2: deadline = 1, profit = 10
+  jobs[2] = make_pair(1, 40); // Job 3: deadline = 1, profit = 40
+  jobs[3] = make_pair(1, 30); // Job 4: deadline = 1, profit = 30
+
+  maxProfit(jobs);
   return 0;
 }
