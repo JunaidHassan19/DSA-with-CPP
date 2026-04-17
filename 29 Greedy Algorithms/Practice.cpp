956e938c4171253f19619b9f6e6381d175dcdc54
JunaidHassan19 <thejunni01@gmail.com>
2026-04-17 22:48:45 +0530

    Practice Maximum Length of Chain of Pairs

--- 29 Greedy Algorithms/Practice.cpp
+++ 29 Greedy Algorithms/Practice.cpp
@@ -56,7 +56,7 @@ int main()
 */
 
 // Practice Fractional Knapsack
-
+/*
 bool compare(pair<double, int> p1, pair<double, int> p2)
 {
   return p1.first > p2.first; // Sort in descending order based on the value-to-weight ratio
@@ -102,5 +102,47 @@ int main()
   int W = 50;
 
   fractionalKnapsack(val, wt, W);
+  return 0;
+}
+*/
+
+// Practice Maximum Length of Chain of Pairs
+bool compare(pair<int, int> p1, pair<int, int> p2)
+{
+  return p1.second < p2.second;
+}
+
+int maxChainLength(vector<pair<int, int>> pairs)
+{
+  int n = pairs.size();
+
+  sort(pairs.begin(), pairs.end(), compare);
+  int ans = 1;
+  int currEnd = pairs[0].second;
+  for (int i = 1; i < n; i++)
+  {
+    if (pairs[i].first > currEnd)
+    {
+      ans++;
+      currEnd = pairs[i].second;
+    }
+  }
+
+  cout << "Max chain Length = " << ans << endl;
+  return ans;
+}
+
+int main()
+{
+  int n = 5;
+  vector<pair<int, int>> pairs(n, make_pair(0, 0));
+
+  pairs[0] = make_pair(5, 24);
+  pairs[1] = make_pair(39, 60);
+  pairs[2] = make_pair(5, 28);
+  pairs[3] = make_pair(27, 40);
+  pairs[4] = make_pair(50, 90);
+
+  maxChainLength(pairs);
   return 0;
 }
