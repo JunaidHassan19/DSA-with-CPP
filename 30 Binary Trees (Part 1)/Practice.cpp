fe02e7ecaf6a787922efb63150ff8665acf270a2
JunaidHassan19 <thejunni01@gmail.com>
2026-04-20 11:39:31 +0530

    Practice -Preorder Traversal of a Binary Tree

--- 30 Binary Trees (Part 1)/Practice.cpp
+++ 30 Binary Trees (Part 1)/Practice.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // Practice Definition of a Node in the Binary Tree
+/*
 
 class Node
 {
@@ -40,5 +41,59 @@ int main()
   Node *root = buildTree(nodes);
 
   cout << "Root Node: " << root->data << endl;
+  return 0;
+}
+*/
+
+// Practice Preorder Traversal of a Binary Tree
+
+class Node
+{
+public:
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int data)
+  {
+    this->data = data;
+    left = right = NULL;
+  }
+};
+
+static int idx = -1;
+Node *buildTree(vector<int> nodes)
+{
+  idx++;
+  if (nodes[idx] == -1)
+  {
+    return NULL;
+  }
+
+  Node *currNode = new Node(nodes[idx]);
+  currNode->left = buildTree(nodes);
+  currNode->right = buildTree(nodes);
+
+  return currNode;
+}
+
+void preorder(Node *root)
+{
+  if (root == NULL)
+  {
+    return;
+  }
+
+  cout << root->data << " ";
+  preorder(root->left);
+  preorder(root->right);
+}
+
+int main()
+{
+  vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+  Node *root = buildTree(nodes);
+
+  preorder(root);
   return 0;
 }
