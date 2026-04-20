7b180ab53c20641d022694c1a6b74747275928d9
JunaidHassan19 <thejunni01@gmail.com>
2026-04-20 10:54:24 +0530

    Practice -Build Tree from Preorder

--- 30 Binary Trees (Part 1)/Practice.cpp
+++ 30 Binary Trees (Part 1)/Practice.cpp
@@ -2,9 +2,43 @@
 #include <vector>
 using namespace std;
 
+// Practice Definition of a Node in the Binary Tree
+
+class Node
+{
+public:
+  int data;
+  Node *left;  // Pointer to the left child
+  Node *right; // Pointer to the right child
+
+  Node(int data)
+  {
+    this->data = data;
+    left = right = NULL;
+  }
+};
+
+static int idx = -1;
+Node *buildTree(vector<int> nodes)
+{
+  idx++;
+  if (nodes[idx] == -1)
+  {
+    return NULL;
+  }
+
+  Node *currNode = new Node(nodes[idx]);
+  currNode->left = buildTree(nodes);
+  currNode->right = buildTree(nodes);
+
+  return currNode;
+}
+
 int main()
 {
   vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+  Node *root = buildTree(nodes);
 
+  cout << "Root Node: " << root->data << endl;
   return 0;
 }
