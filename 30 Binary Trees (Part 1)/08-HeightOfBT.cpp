9c445af3ae3ff9546ebbef86b7b2085b0c05c63d
JunaidHassan19 <thejunni01@gmail.com>
2026-04-20 22:48:32 +0530

    Height of Binary Tree

--- 30 Binary Trees (Part 1)/08-HeightOfBT.cpp
+++ 30 Binary Trees (Part 1)/08-HeightOfBT.cpp
@@ -47,7 +47,8 @@ int height(Node *root)
 
 int main()
 {
-  vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+  // vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+  vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, 6, -1, 7, -1, -1, 3, -1, -1};
   Node *root = buildTree(nodes);
 
   cout << "Height = " << height(root) << endl;
