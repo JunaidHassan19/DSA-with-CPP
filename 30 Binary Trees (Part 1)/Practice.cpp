3d542cb020afab4a16f4b557ae5cbb744fd7bb41
JunaidHassan19 <thejunni01@gmail.com>
2026-04-21 22:23:10 +0530

    Practice -Sum of Nodes in a Binary Tree

--- 30 Binary Trees (Part 1)/Practice.cpp
+++ 30 Binary Trees (Part 1)/Practice.cpp
@@ -203,6 +203,16 @@ int count(Node *root)
 */
 
 // Practice -Sum of Nodes in a Binary Tree
+// int sum(Node *root)
+// {
+//   if (root == NULL)
+//   {
+//     return 0;
+//   }
+//   int leftSum = sum(root->left);
+//   int rightSum = sum(root->right);
+//   return leftSum + rightSum + root->data;
+// }
 int sum(Node *root)
 {
   if (root == NULL)
