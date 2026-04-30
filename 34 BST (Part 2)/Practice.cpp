9e325ff0e3ff4a7b22a48618e136f46a5c3def17
JunaidHassan19 <thejunni01@gmail.com>
2026-04-30 21:32:10 +0530

    Practice -Largest BST in a Binary Tree

--- 34 BST (Part 2)/Practice.cpp
+++ 34 BST (Part 2)/Practice.cpp
@@ -82,6 +82,51 @@ Node *balanceBST(Node *root)
   return BSTfromSortedVec(nodes, 0, nodes.size() - 1);
 }
 
+// Practice -Largest BST in a Binary Tree
+
+class Info
+{
+public:
+  bool isBST;
+  int min;
+  int max;
+  int sz;
+
+  Info(bool isBST, int min, int max, int sz)
+  {
+    this->isBST = isBST;
+    this->min = min;
+    this->max = max;
+    this->sz = sz;
+  }
+};
+
+static int maxSize;
+
+Info *largestBST(Node *root)
+{
+  if (root == NULL)
+  {
+    return new Info(true, INT_MAX, INT_MIN, 0);
+  }
+
+  Info *leftInfo = largestBST(root->left);
+  Info *rightInfo = largestBST(root->right);
+
+  int currMin = min(root->data, min(leftInfo->min, rightInfo->min));
+  int currMax = max(root->data, max(leftInfo->max, rightInfo->max));
+  int currSz = leftInfo->sz + rightInfo->sz + 1;
+
+  if (leftInfo->isBST && rightInfo->isBST && root->data > leftInfo->max && root->data < rightInfo->min)
+  {
+    maxSize = max(maxSize, currSz);
+    return new Info(true, currMin, currMax, currSz);
+  }
+
+  maxSize = max(maxSize, max(leftInfo->sz, rightInfo->sz));
+  return new Info(false, currMin, currMax, currSz);
+}
+
 int main()
 {
   // int arr[7] = {3, 4, 5, 6, 7, 8, 9};
@@ -89,18 +134,31 @@ int main()
   // preorder(root);
   // cout << endl;
 
-  Node *root = new Node(6);
-  root->left = new Node(5);
-  root->left->left = new Node(4);
-  root->left->left->left = new Node(3);
+  // Node *root = new Node(6);
+  // root->left = new Node(5);
+  // root->left->left = new Node(4);
+  // root->left->left->left = new Node(3);
 
-  root->right = new Node(7);
-  root->right->right = new Node(8);
-  root->right->right->right = new Node(9);
+  // root->right = new Node(7);
+  // root->right->right = new Node(8);
+  // root->right->right->right = new Node(9);
 
-  root = balanceBST(root);
-  preorder(root);
-  cout << endl;
+  // root = balanceBST(root);
+  // preorder(root);
+  // cout << endl;
 
+  Node *root = new Node(50);
+  root->left = new Node(30);
+  root->left->left = new Node(5);
+  root->left->left->left = new Node(20);
+
+  root->right = new Node(60);
+  root->right->right = new Node(45);
+  root->right->right->right = new Node(70);
+  root->right->right->right->left = new Node(65);
+  root->right->right->right->right = new Node(80);
+
+  largestBST(root);
+  cout << "Max Size : " << maxSize << endl;
   return 0;
 }
