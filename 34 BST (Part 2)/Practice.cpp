f79ea343d9ddfa92fc99db32fc6d75223b140c97
JunaidHassan19 <thejunni01@gmail.com>
2026-04-30 20:12:13 +0530

    Prctice -BST to Balanced BST

--- 34 BST (Part 2)/Practice.cpp
+++ 34 BST (Part 2)/Practice.cpp
@@ -45,11 +45,60 @@ void preorder(Node *root)
   preorder(root->right);
 }
 
+// Prctice -BST to Balanced BST
+Node *BSTfromSortedVec(vector<int> arr, int st, int end)
+{
+  if (st > end)
+  {
+    return NULL;
+  }
+
+  int mid = st + (end - st) / 2;
+  Node *curr = new Node(arr[mid]);
+
+  curr->left = BSTfromSortedVec(arr, st, mid - 1);
+  curr->right = BSTfromSortedVec(arr, mid + 1, end);
+
+  return curr;
+}
+
+void getInorder(Node *root, vector<int> &nodes)
+{
+  if (root == NULL)
+  {
+    return;
+  }
+
+  getInorder(root->left, nodes);
+  nodes.push_back(root->data);
+  getInorder(root->right, nodes);
+}
+
+Node *balanceBST(Node *root)
+{
+  vector<int> nodes;
+  getInorder(root, nodes);
+
+  return BSTfromSortedVec(nodes, 0, nodes.size() - 1);
+}
+
 int main()
 {
-  int arr[7] = {3, 4, 5, 6, 7, 8, 9};
+  // int arr[7] = {3, 4, 5, 6, 7, 8, 9};
+  // Node *root = buildBSTfromSorted(arr, 0, 6);
+  // preorder(root);
+  // cout << endl;
 
-  Node *root = buildBSTfromSorted(arr, 0, 6);
+  Node *root = new Node(6);
+  root->left = new Node(5);
+  root->left->left = new Node(4);
+  root->left->left->left = new Node(3);
+
+  root->right = new Node(7);
+  root->right->right = new Node(8);
+  root->right->right->right = new Node(9);
+
+  root = balanceBST(root);
   preorder(root);
   cout << endl;
 
