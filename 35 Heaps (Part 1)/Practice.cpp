d40c0f27826d6e5acf6f763c729e24eb5d8fa609
JunaidHassan19 <thejunni01@gmail.com>
2026-05-03 17:18:29 +0530

    Practice -Push in Heap

--- 35 Heaps (Part 1)/Practice.cpp
+++ 35 Heaps (Part 1)/Practice.cpp
@@ -32,6 +32,19 @@ class Heap
   vector<int> vec;
 
 public:
+  // Practice -Push in Heap
+  // void push(int val)
+  // {
+  //   vec.push_back(val);
+  //   int x = vec.size() - 1;
+  //   int parI = (x - 1) / 2;
+  //   while (parI >= 0 && vec[x] > vec[parI])
+  //   {
+  //     swap(vec[x], vec[parI]);
+  //     x = parI;
+  //     parI = (x - 1) / 2;
+  //   }
+  // }
   void push(int val)
   {
     vec.push_back(val);
