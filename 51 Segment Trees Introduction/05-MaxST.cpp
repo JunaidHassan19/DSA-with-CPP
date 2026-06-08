1902d88e138c9a22c35990e2657704c64e554f5e
JunaidHassan19 <thejunni01@gmail.com>
2026-06-08 21:35:57 +0530

    Maximum Segment Tree

--- 51 Segment Trees Introduction/05-MaxST.cpp
+++ 51 Segment Trees Introduction/05-MaxST.cpp
@@ -47,6 +47,29 @@ class SegmentTree
     return max(leftMax, rightMax);
   }
 
+  // Function to perform point update on the segment tree
+  void updateUtil(int idx, int newVal, int start, int end, int node)
+  {
+    if (start == end)
+    {
+      tree[node] = newVal;
+      return;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    if (idx >= start && idx <= mid)
+    {
+      updateUtil(idx, newVal, start, mid, 2 * node + 1);
+    }
+    else
+    {
+      updateUtil(idx, newVal, mid + 1, end, 2 * node + 2);
+    }
+
+    tree[node] = max(tree[2 * node + 1], tree[2 * node + 2]);
+  }
+
 public:
   SegmentTree(vector<int> &nums)
   {
@@ -69,6 +92,12 @@ public:
   {
     return maxQuery(qi, qj, 0, n - 1, 0);
   }
+
+  // Public function to update a value in the segment tree
+  void update(int idx, int newVal)
+  {
+    updateUtil(idx, newVal, 0, n - 1, 0);
+  }
 };
 
 int main()
@@ -78,5 +107,9 @@ int main()
   SegmentTree st(nums);
   st.printTree();
   cout << "Maximum in range [2, 5]: " << st.rangeMaxQuery(2, 5) << endl;
+
+  st.update(4, 5); // Update index 4 to value 5
+  cout << "Maximum in range [0, 8] after update: " << st.rangeMaxQuery(0, 8) << endl;
+  st.printTree();
   return 0;
 }
